f3ef94fe1596b9900aed7ea69acc616367b98ced
Justin Rowsell <justin.rowsell@aquaberry.io>
2025-01-17 10:11:36 -0500

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,56 +1,10 @@
-import azure from 'svelte-adapter-azure-swa';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: azure({
-			customStaticWebAppConfig: {
-				routes: [
-					{
-						"route": "/login",
-						"rewrite": "/.auth/login/aad?post_login_redirect_uri=/events",
-						"allowedRoles": ["anonymous", "authenticated"]
-					},
-					{
-						"route": "/.auth/login/github",
-						"statusCode": 404
-					},
-					{
-						"route": "/.auth/login/twitter",
-						"statusCode": 404
-					},
-					{
-						"route": "/logout",
-						"redirect": "/.auth/logout",
-						"allowedRoles": ["anonymous", "authenticated"]
-					},
-					{
-						"route": "/events/*",
-						"allowedRoles": ["authenticated"]
-					},
-					{
-						"route": "/customers/*",
-						"allowedRoles": ["authenticated"]
-					},
-					{
-						"route": "/admin/*",
-						"allowedRoles": ["authenticated"]
-					}
-				],
-				responseOverrides: {
-					'401': {
-						'redirect': '/login',
-						'statusCode': 302
-					}
-				}
-			}
-		})
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
-
 export default config;
